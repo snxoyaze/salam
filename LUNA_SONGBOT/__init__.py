e549f423b25571d20c253510873e9508af1a0b83
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-13 12:34:50 +0530

    Update __init__.py

--- LUNA_SONGBOT/__init__.py
+++ LUNA_SONGBOT/__init__.py
@@ -9,15 +9,7 @@ import logging
 
 
 
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
 
-logger = logging.getLogger(__name__)
-logger.info(msg="Logging started...")
-
-LOG = logging.getLogger(__name__)
 GENIUS ="false"
 
 
