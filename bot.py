4f0213d4b35651db7d4e2c261c177e3c19a72bf7
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-13 12:39:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,25 +8,24 @@ logging.getLogger().setLevel(logging.ERROR)
 
 
 
-class Bot(Client):
+class pbot(Client):
 
     def __init__(self):
         super().__init__(
-            session_name=SESSION,
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
+            bot_token = os.environ["BOT_TOKEN"],
+            api_id = int(os.environ["API_ID"]),
+            api_hash = os.environ["API_HASH"], 
             workers=50,
-            plugins={"root": "plugins"},
+            plugins={"root": "luna_songbot"},
             sleep_threshold=5,
         )
 
-        print(f"LUNA started on {me.username}.")
+        
 
     async def stop(self, *args):
         await super().stop()
         print("Bot stopped. Bye.")
 
 
-app = Bot()
+app = pbot()
 app.run()
