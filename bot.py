1268d404a37ae1edfe820294a35d4fe468045dc4
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-13 12:29:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,7 @@ import logging.config
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.ERROR)
 
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
-from database.users_chats_db import db
+
 
 
 class Bot(Client):
