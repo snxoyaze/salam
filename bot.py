5f28d61fa2942e568e95d245cbc29a1fdb783d24
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-14 10:03:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,8 @@ import os
 from os import environ
 from pyrogram import Client, filters
 import logging
+from config import Config
 import pyrogram
-
-
-#config
-BOT_TOKEN = environ['BOT_TOKEN']
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-
 
 
 #  logging
